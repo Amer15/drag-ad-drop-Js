dfc343d2c29a1c72ce53019cb86fdcbed1a84651
AMER <amer95521@gmail.com>
2020-10-22 15:44:54 +0530

    updated event listener methods

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
-const images = document.querySelectorAll('img');
-const boxes = document.querySelectorAll('div');
+const images = document.querySelectorAll("img");
+const boxes = document.querySelectorAll("div");
 
 //Getting img urls of drag and drop image
-let dragImgSrc = '';
-let dropImgSrc = '';
+let dragImgSrc = "";
+let dropImgSrc = "";
+
+//Getting Ids of drag and drop image
+let dragImgId = "";
+let dropImgId = "";
 
 //adding drag event listeners on each img tag
-images.forEach( img => {
-    img.addEventListener('dragstart', (e) => {
-        //getting the src of image which is going to be dragged
-        dragImgSrc = e.target.getAttribute('src');
-    });
+images.forEach((img) => {
+  img.addEventListener("dragstart", (e) => {
+    //getting the src of image and also id of img tag which is dragged
+    //and storing them in drag variables
+    dragImgSrc = e.target.getAttribute("src");
+    dragImgId = e.target.id;
+  });
 
-    img.addEventListener('dragend', (e) => {
-        e.target.setAttribute('src', dropImgSrc);
-        droppedImgSrc = '';
-        dragImgSrc = '';
-    });
+  img.addEventListener("dragend", (e) => {
+    //checking whether both the dragged and drop img elements are same  
+    //If they are different performing swap (Drag and Drop)
+    if (dragImgId !== dropImgId && dragImgSrc !== dropImgSrc) {
+      let hoveredImg = document.getElementById(dropImgId);
+      hoveredImg.setAttribute("src", dragImgSrc);
+      let draggedImg = document.getElementById(dragImgId);
+      draggedImg.setAttribute("src", dropImgSrc);
+    }
+    //Setting to variables to empty strings after perfoming swap
+    dragImgSrc = "";
+    dropImgSrc = "";
+    dragImgId = "";
+    dropImgId = "";
+  });
 });
 
+//Attaching event listeners on every img for dragover and drop events
+images.forEach((image) => {
+  image.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
 
-//adding drag event listeners on each div
-boxes.forEach( box => {
-    box.addEventListener('dragover', (e) => {
-        e.preventDefault();
-        //getting the src of image which is going to be swapped
-        dropImgSrc = e.target.getAttribute('src');
-    });
-
-    box.addEventListener('drop', (e) => {
-        e.target.setAttribute('src', dragImgSrc);
-    });
-
+  image.addEventListener("drop", (e) => {
+    //getting the src of image and also id of img tag which is hovered by dragged img
+    //and storing them in drop variables 
+    dropImgSrc = e.target.getAttribute("src");
+    dropImgId = e.target.id;
+  });
 });
-
